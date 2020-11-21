b9d70f2695ff3fe955c9602a08e727341692811e
Esteban <esteban@estebans-mbp.lan>
2020-11-21 00:13:32 -0600

    completed MVP crud

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
-
+const data = require('../data/data');
+const shortid = require('shortid');
+const e = require('express');
 //middleware
 const server = express();
 server.use(express.json());
@@ -10,16 +12,94 @@ server.use(express.json());
 
 server.get('/api', (req, res)=>{
 
-    res.json({message: 'Welcome! request an endpoint to get started!'})
+    res.status(200).json({message: 'Welcome! request an endpoint to get started!'});
 
 })
 
-//POST
+//POST - creates new user
 server.post('/api/users', (req, res) =>{
+    if (req.body.bio && req.body.name !== null ){
+        const newUser = {
+            id: shortid.generate(),
+            name: req.body.name,
+            bio: req.body.bio
+        }
+    
+        data.push(newUser);
+        console.log(`new user was created : `, newUser)
+        res.json(newUser)
+    }else{
+       res.status(500).json(`Please include both a new user name and bio`)
+    }
 
-    console.log(req.url, req.headers['user-agent'], req.body)
 
 })
 
+//GET 
+server.get('/api/users', (req, res) => {
+
+
+    res.json(data)
+})
+
+//GET - gets user by id
+server.get('/api/users/:id', async (req, res)=>{
+    try{
+        const userIndex = await data.findIndex( function(user){
+            return user.id === req.params.id
+        })
+
+        if(req.params.id === data[userIndex].id ){
+            res.json(data[userIndex])
+            console.log(`User has been found! : `, data[userIndex])
+
+        }
+        
+    }catch(err){
+        res.json({errorMessage: `${err}`})
+    }
+
+    
+
+})
+
+//DELETE
+server.delete('/api/users/:id', async (req, res)=>{
+    try{
+        const userIndex = await data.findIndex( function(user){
+            return user.id === req.params.id
+        })
+
+        if(req.params.id === data[userIndex].id ){
+            data.slice(data[userIndex], data[userIndex]++)
+            res.json(data[userIndex])
+            console.log(`User has been found and is set to delete from user list!`)
+
+        }
+        
+    }catch(err){
+        res.json({errorMessage: `${err}`})
+    }
+
+})
+
+//PUT
+server.put('/api/users/:id', async (req, res)=>{
+    try{
+        const userIndex = await data.findIndex( function(user){
+            return user.id === req.params.id
+        })
+        
+        data[userIndex].name = req.body.newName
+        data[userIndex].bio = req.body.newBio
+    }catch(err){
+        res.json({errorMessage: `${err}`})
+    }finally{
+        
+        res.json({message: 'success'})
+    }
+
+})
 
 module.exports = server; 
+
